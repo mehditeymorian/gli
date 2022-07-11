0f25155385c1421a2ad5a70d80d0bb9064ecb507
Mehdi <mehditeymorian322@gmail.com>
2022-07-11 21:11:29 +0430

    feat: add question package

---
+++ internal/question/questions.go
@@ -0,0 +1,79 @@
+package question
+
+import (
+	"github.com/AlecAivazis/survey/v2"
+	"github.com/mehditeymorian/gli/internal/config"
+	"github.com/mehditeymorian/gli/internal/model"
+)
+
+type Question struct {
+	Config config.Config
+}
+
+func New(cfg config.Config) Question {
+	return Question{
+		Config: cfg,
+	}
+}
+
+func (q Question) Fill(app *model.App) {
+	err := survey.Ask(InitialQuestions(q.Config), app)
+	handleErr(err)
+
+	err = survey.Ask(ModuleQuestions(app.Modules, q.Config), app)
+	handleErr(err)
+
+	err = survey.AskOne(DockerfileQuestion(), &app.Dockerfile)
+	handleErr(err)
+}
+
+func InitialQuestions(cfg config.Config) []*survey.Question {
+	return []*survey.Question{
+		{
+			Name:   "Name",
+			Prompt: &survey.Input{Message: "App Name?"},
+		},
+		{
+			Name: "Version",
+			Prompt: &survey.Select{
+				Message: "Go Version",
+				Options: cfg.Versions,
+			},
+		},
+		{
+			Name: "modules",
+			Prompt: &survey.MultiSelect{
+				Message: "choose module you need",
+				Options: cfg.ModuleNames(),
+			},
+		},
+	}
+}
+
+func ModuleQuestions(selectedModules []string, cfg config.Config) []*survey.Question {
+	moduleQuestions := make([]*survey.Question, 0)
+	for _, module := range selectedModules {
+		moduleQuestions = append(moduleQuestions, &survey.Question{
+			Name: module,
+			Prompt: &survey.Select{
+				Message: "choose technology for " + module,
+				Options: cfg.Modules[module],
+			},
+		})
+	}
+
+	return moduleQuestions
+}
+
+func DockerfileQuestion() *survey.Confirm {
+	return &survey.Confirm{
+		Default: false,
+		Message: "Do you want to create a Dockerfile?",
+	}
+}
+
+func handleErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
