c677a3eb3a9294e1718414eef80d1546472db655
Mehdi <mehditeymorian322@gmail.com>
2022-07-11 21:11:50 +0430

    feat: fill app request

--- internal/cmd/create.go
+++ internal/cmd/create.go
@@ -1,6 +1,12 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"github.com/mehditeymorian/gli/internal/config"
+	"github.com/mehditeymorian/gli/internal/model"
+	"github.com/mehditeymorian/gli/internal/question"
+	"github.com/spf13/cobra"
+	"log"
+)
 
 func Create() *cobra.Command {
 	cmd := &cobra.Command{
@@ -14,5 +20,11 @@ func Create() *cobra.Command {
 }
 
 func run(_ *cobra.Command, _ []string) {
-	
+	cfg := config.Load()
+
+	app := model.EmptyApp()
+
+	question.New(cfg).Fill(app)
+
+	log.Println(app)
 }
