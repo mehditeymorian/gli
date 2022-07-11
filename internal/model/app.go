9894f2a1d157c013fe80b666d956eb35af533251
Mehdi <mehditeymorian322@gmail.com>
2022-07-11 21:10:51 +0430

    feat: add dockerfile

--- internal/model/app.go
+++ internal/model/app.go
@@ -3,9 +3,14 @@ package model
 type App struct {
 	Modules []string
 
-	Name    string
-	Version string
-	Logger  string
-	DB      string
-	HTTP    string
+	Name       string
+	Version    string
+	Logger     string
+	DB         string
+	HTTP       string
+	Dockerfile bool
+}
+
+func EmptyApp() *App {
+	return &App{}
 }
