fa7572964873543fcdcf0a0379800d7cdb623229
Mehdi <mehditeymorian322@gmail.com>
2022-07-15 12:48:43 +0430

    feat: improve error handling

--- internal/question/questions.go
+++ internal/question/questions.go
@@ -1,6 +1,7 @@
 package question
 
 import (
+	"fmt"
 	"github.com/AlecAivazis/survey/v2"
 	"github.com/mehditeymorian/gli/internal/config"
 	"github.com/mehditeymorian/gli/internal/model"
@@ -18,13 +19,13 @@ func New(cfg config.Config) Question {
 
 func (q Question) Fill(app *model.App) {
 	err := survey.Ask(InitialQuestions(q.Config), app)
-	handleErr(err)
+	handleErr("failed to ask general questions", err)
 
 	err = survey.Ask(ModuleQuestions(app.Modules, q.Config), app)
-	handleErr(err)
+	handleErr("failed to ask modules", err)
 
 	err = survey.AskOne(DockerfileQuestion(), &app.Dockerfile)
-	handleErr(err)
+	handleErr("failed to ask dockerfile", err)
 }
 
 func InitialQuestions(cfg config.Config) []*survey.Question {
@@ -57,7 +58,7 @@ func ModuleQuestions(selectedModules []string, cfg config.Config) []*survey.Ques
 			Name: module,
 			Prompt: &survey.Select{
 				Message: "choose technology for " + module,
-				Options: cfg.Modules[module],
+				Options: cfg.ModuleOptions(module),
 			},
 		})
 	}
@@ -72,8 +73,8 @@ func DockerfileQuestion() *survey.Confirm {
 	}
 }
 
-func handleErr(err error) {
+func handleErr(message string, err error) {
 	if err != nil {
-		panic(err)
+		panic(fmt.Errorf("%s: %w", message, err))
 	}
 }
