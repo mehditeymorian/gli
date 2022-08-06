47d2ec660fbfd23b12c4a384ffa432b7227678ab
Mehdi Teymorian <mehditeymorian322@gmail.com>
2022-08-06 23:47:37 +0430

    feat: add downloadURL and SavePath to module

--- internal/model/module.go
+++ internal/model/module.go
@@ -1,7 +1,31 @@
 package model
 
+import "path"
+
 type Module struct {
-	Name    string
+	// Name of the module
+	Name string
+
+	// DownloadURL location of module in the template to download from
+	DownloadURL string
+
+	// SavePath path that files are stored
+	SavePath []string
+
+	// Package module package to download
 	Package string
-	Files   []string
+
+	Files []ModuleFile
+}
+
+func (m Module) GetSavePath(parentDirectory string) string {
+	join := make([]string, len(m.SavePath)+1)
+
+	join[0] = parentDirectory
+
+	for i, each := range m.SavePath {
+		join[i+1] = each
+	}
+
+	return path.Join(join...)
 }
