89f1c56d6ccbbc4b691f1674778f826d582edb09
Mehdi Teymorian <mehditeymorian322@gmail.com>
2022-08-11 11:29:47 +0430

    feat: add start point constants

--- internal/model/constants.go
+++ internal/model/constants.go
@@ -1,11 +1,14 @@
 package model
 
 const (
-	None       = "none"
-	DB         = "db"
-	HTTP       = "http"
-	Logger     = "logger"
-	Dockerfile = "Dockerfile"
+	None             = "none"
+	DB               = "db"
+	HTTP             = "http"
+	Logger           = "logger"
+	Dockerfile       = "Dockerfile"
+	StartPoint       = "StartPoint"
+	StartPointCli    = "Cli"
+	StartPointSimple = "Simple"
 )
 
 type Flag string
