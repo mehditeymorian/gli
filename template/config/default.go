e45dec83daae46e3a8953d69f942de45e401cc8a
Mehdi Teymorian <mehditeymorian322@gmail.com>
2022-08-16 23:02:43 +0430

    feat: add logger default config template

--- template/config/default.go
+++ template/config/default.go
@@ -2,12 +2,16 @@ package config
 
 import (
 	{{if .HasHTTP}}"{{.Name}}/internal/http"{{end}}
+	{{if .HasLogger}}"{{.Name}}/internal/logger"{{end}}
 )
 
 func Default() Config {
 	return Config{
 		{{if .HasHTTP}}HTTP: http.Config{
 			Port: "8080",
+	},{{end}}
+		{{if .HasLogger}}Logger: logger.Config{
+			Level: "warn",
 	},{{end}}
 	}
 }
