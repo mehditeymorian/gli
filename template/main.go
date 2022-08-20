baae4caa9a07990fd5bdda4a7e1b06b416eb3183
Mehdi Teymorian <mehditeymorian322@gmail.com>
2022-08-20 10:34:34 +0430

    feat: add mongo initialization to simple entry point

--- template/main.go
+++ template/main.go
@@ -4,11 +4,22 @@ import (
 	"{{ .Name }}/internal/config"
 	{{if .HasLogger}}"{{ .Name }}/internal/logger"{{end}}
 	{{if .HasHTTP}}"{{ .Name }}/internal/http/app"{{end}}
+	{{if .HasDB}}"{{ .Name }}/internal/db"{{end}}
 )
 
 func main() {
 	cfg := config.Load("config.yaml")
+
 	{{if .HasLogger}}log := logger.New(cfg.Logger){{end}}
+
+	{{if .HasDB}}
+	{{if eq .DB "mongo"}} database, err := db.Connect(cfg.DB){{end}}
+
+	if err != nil{
+		log.Fatalf("failed to connect to db: %v", err)
+	}
+	{{end}}
+
 	{{if .HasHTTP}}app := app.New(cfg.HTTP){{end}}
 
 	{{if .HasHTTP}}app.Serve(){{end}}
