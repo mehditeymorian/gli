323e7b49c8d918bdc8eae07353afd747c6ee410a
Mehdi Teymorian <mehditeymorian322@gmail.com>
2022-08-20 10:33:48 +0430

    feat: add mongo initialization to cli entry point

--- template/cli/command.go
+++ template/cli/command.go
@@ -5,6 +5,7 @@ import (
 	"{{ .Name }}/internal/config"
 	{{if .HasLogger}}"{{ .Name }}/internal/logger"{{end}}
 	{{if .HasHTTP}}"{{ .Name }}/internal/http/app"{{end}}
+	{{if .HasDB}}"{{ .Name }}/internal/db"{{end}}
 )
 
 func {{ capitalize .CliName }}() *cobra.Command {
@@ -21,7 +22,17 @@ func {{ capitalize .CliName }}() *cobra.Command {
 
 func main(cmd *cobra.Command, args []string)  {
 	cfg := config.Load("config.yaml")
+
 	{{if .HasLogger}}log := logger.New(cfg.Logger){{end}}
+
+	{{if .HasDB}}
+	{{if eq .DB "mongo"}} database, err := db.Connect(cfg.DB){{end}}
+
+	if err != nil{
+		log.Fatalf("failed to connect to db: %v", err)
+	}
+	{{end}}
+
 	{{if .HasHTTP}}app := app.New(cfg.HTTP){{end}}
 
 	{{if .HasHTTP}}app.Serve(){{end}}
